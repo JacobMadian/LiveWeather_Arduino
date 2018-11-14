6e629387890e045140e357f2fb644dd7ea1730be
Jacob Madian <44143326+JacobMadian@users.noreply.github.com>
2018-11-13 21:21:13 -0800

    Added initial test code to learn Arduino IDE

--- Weather_Arduino/Weather_Arduino.ino
+++ Weather_Arduino/Weather_Arduino.ino
@@ -1,9 +1,11 @@
+const int AnalogIn = A0;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  readingIn = analogRead(AnalogIn);
+  Serial.println(readingIn);
 }
