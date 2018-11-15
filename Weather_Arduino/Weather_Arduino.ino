26e04c7be42ecd062d7dcdcaaa4f0be0886fad2b
Jacob Madian <44143326+JacobMadian@users.noreply.github.com>
2018-11-14 20:25:10 -0800

    Added FastLED Library and began development of intial LED code

--- Weather_Arduino/Weather_Arduino.ino
+++ Weather_Arduino/Weather_Arduino.ino
@@ -1,11 +1,24 @@
-const int AnalogIn = A0;
+#include <FastLED.h>
+#define NUM_LEDS 24
+#define LED_PIN 2
+
+CRGB leg[NUM_LEDS];
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  FastLED.addLEDS<NEOPIXEL, LED_PIN>(led, NUM_LEDS);
+
+    for(int i = 0; i < NUM_LEDS; i++) {
+      led[i] = CRGB(0, 0, 255);
+    }
+
+    FastLED.show();
+}
+
+void rain(int val) {
+  for 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  readingIn = analogRead(AnalogIn);
-  Serial.println(readingIn);
+
 }
